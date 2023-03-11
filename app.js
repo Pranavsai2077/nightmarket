2e21a263fec900bb614729198ffaf17e0de19f2e
Pranav <75947452+Pranavsai2077@users.noreply.github.com>
2023-03-11 12:10:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('Feb 20, 2023 00:00:00').getTime();
+    const countDate = new Date('Feb 20, 2024 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
